df4d155d36e409a74ff6ee11bf497bb22954212c
Bruno Deferrari <utizoc@gmail.com>
2022-10-29 10:44:42 -0300

    More options for loading the block data json into run_verifier

--- src/app/run_verifier/run_verifier.ml
+++ src/app/run_verifier/run_verifier.ml
@@ -1,10 +1,43 @@
-[%%import "/src/config.mlh"]
-
 open Core
 open Async
 open Blockchain_snark
 open Mina_base
 open Mina_state
+
+let parse_json_line_or_file file_or_json =
+  let json_string =
+    if String.is_prefix file_or_json ~prefix:"@" then
+      let filename = String.drop_prefix file_or_json 1 in
+      In_channel.read_all filename
+    else file_or_json
+  in
+  match
+    [%derive.of_yojson: Blockchain_snark.Blockchain.t list]
+      (Yojson.Safe.from_string json_string)
+  with
+  | Ok input ->
+      input
+  | Error err ->
+      Stdlib.Printf.printf "Could not parse JSON: %s" err ;
+      Stdlib.exit 1
+
+let get_input () =
+  let input =
+    match Sys.get_argv () with
+    | [| _; filename |] ->
+        parse_json_line_or_file ("@" ^ filename)
+    | _ ->
+        Stdlib.Printf.printf
+          "No block json file provided, please input JSON (single line) or \
+           @path/to/file.json:\n\
+           %!" ;
+        let input_line = Stdlib.input_line Stdlib.stdin in
+        Stdlib.Printf.printf "Parsing.\n%!" ;
+        parse_json_line_or_file input_line
+  in
+  List.map input ~f:(fun snark ->
+      ( Blockchain_snark.Blockchain.state snark
+      , Blockchain_snark.Blockchain.proof snark ) )
 
 let run () =
   let proof_level = Genesis_constants.Proof_level.compiled in
@@ -31,24 +64,7 @@ let run () =
   let after_time = Unix.gettimeofday () in
   Stdlib.Printf.printf "Blockchain snark module creation time: %fs\n%!"
     (after_time -. before_time) ;
-  Stdlib.Printf.printf "Input JSON:\n%!" ;
-  let input_line = Stdlib.input_line Stdlib.stdin in
-  Stdlib.Printf.printf "Parsing.\n%!" ;
-  let input =
-    match
-      [%derive.of_yojson: Blockchain_snark.Blockchain.t list]
-        (Yojson.Safe.from_string input_line)
-    with
-    | Ok input ->
-        input
-    | Error err ->
-        failwithf "Could not parse JSON: %s" err ()
-  in
-  let x =
-    List.map input ~f:(fun snark ->
-        ( Blockchain_snark.Blockchain.state snark
-        , Blockchain_snark.Blockchain.proof snark ) )
-  in
+  let input = get_input () in
   let verify : (Protocol_state.Value.t * Proof.t) list -> bool =
     B.Proof.verify
   in
@@ -56,7 +72,7 @@ let run () =
   let before_time = Unix.gettimeofday () in
   Stdlib.Printf.printf "Breakpoint before calling verify: break @ %s %d\n%!"
     __MODULE__ __LINE__ ;
-  let result = verify x in
+  let result = verify input in
   let after_time = Unix.gettimeofday () in
   Stdlib.Printf.printf "Verification time: %fs\n%!" (after_time -. before_time) ;
   match result with
